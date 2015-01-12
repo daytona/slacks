16cba08ae859ce397809f886771256b2cf23cf46
Carl Törnqvist <calle.tornqvist@gmail.com>
2015-01-12 22:23:49 +0100

    Refactored middleware

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
 var koa = require('koa');
 var body = require('koa-body');
-var app = koa();
+var app = module.exports = koa();
 
 // Load environment variables
-if (process.env.NODE_ENV === 'development') {
+if (app.env === 'development') {
   require('node-env-file')(__dirname + '/.env');
 }
 
 app.use(body());
 
-// Read request
-app.use(function *(next){
+/**
+ * Handshake-ish
+ */
+app.use(function *(next) {
+  var body = this.request.body;
+  var received = new Date();
+
+  yield next;
+
+  if (this.valid && this.text) {
+    socket.emit('message', {
+      text: this.text,
+      name: body.user_name,
+      timestamp: body.timestamp,
+      delayed: (received - new Date(body.timestamp))
+    });
+  }
+});
+
+/**
+ * Valid, are ya?
+ */
+app.use(function *(next) {
   var body = this.request.body;
   var token = process.env.SLACK_TOKEN;
-  var text = body.text;
 
-  if ((token && (body.token !== token)) || (body.user_name === 'slackbot')) {
-    yield next;
-  }
+  this.valid = (!token || (token && (body.token !== token)));
+
+  yield next;
+
+  this.valid = (this.valid && this.text);
+});
+
+/**
+ * Parsage
+ */
+app.use(function *(next) {
+  var body = this.request.body;
 
   if (body.trigger_word) {
-    text = body.text.replace(body.trigger_word, '').trim();
+    this.text = body.text.replace(body.trigger_word, '').trim();
+  } else {
+    this.text = body.text;
   }
 
-  this.body = {
-    text: ('Simon said: "' + text + '"')
-  };
+  yield next;
+});
 });
 
 app.listen(process.env.PORT);
