afd7e453c65a5c65ae88b31b8057a2cbeda60ccc
Carl Törnqvist <calle.tornqvist@gmail.com>
2015-01-12 22:26:06 +0100

    Added static example page

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var koa = require('koa');
 var body = require('koa-body');
 var socket = require('koa-socket');
+var serve = require('koa-static');
 var app = module.exports = koa();
 
 // Load environment variables
 if (app.env === 'development') {
   require('node-env-file')(__dirname + '/.env');
+
+  app.use(serve('.'));
 }
 
 app.use(body());
