268cb4591cd75e74271ae0e33cb98f043e5334b1
Vincent Orback <vorback@gmail.com>
2015-04-15 13:25:42 +0200

    Initial commit

---
+++ client.js
@@ -0,0 +1,53 @@
+(function (window) {
+  'use strict';
+
+  var socket = io('http://localhost:1234');
+
+  var $chat = $('#chat');
+  var $chatForm = $('#chat-form');
+  var $chatText = $('#chat-message');
+  var $chatUsername = $('#chat-username');
+
+  $chatText.focus();
+
+  $chatForm.on('submit', function (e) {
+    var message = $chatText.val();
+
+    if (message.length) {
+      socket.emit('message', {
+        message: $chatText.val(),
+        username: $chatUsername.val()
+      });
+      $chatText.val('');
+    } else {
+      alert('You must write a message first!!');
+    }
+
+    e.preventDefault();
+    return false;
+  });
+
+
+  // CMD + Enter = Submit
+  $(document.body).on('keydown', function (e) {
+    if (e.keyCode == 13 && e.metaKey) {
+      $chatForm.submit();
+    }
+  });
+
+
+  socket.on('chat', function (data) {
+    var date = new Date(),
+    niceDate = date.getHours() + ':' + date.getMinutes();
+
+    var message = '<div class="Chat-message" style="display:none">';
+        message += '<p>' + data.message + '</p>';
+        message += '<p class="Chat-messageMeta">' + niceDate + '</p>';
+        message += '</div>';
+    
+    var $message = $(message);
+    $chat.append($message);
+    $message.slideDown('fast');
+  });
+
+}(this));
---
+++ index.js
@@ -0,0 +1,74 @@
+var util = require('util');
+var _ = require('lodash');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var request = require('request');
+var app = module.exports = express();
+
+
+
+// Define app properties
+app.set('port', process.env.PORT || 1234);
+
+
+// Serve static files from project root
+app.use('/', express.static(__dirname + '/'));
+
+
+// Set up session
+app.use(cookieParser());
+app.use(session({
+  name: 'daytona-chat',
+  secret: _.uniqueId(),
+  saveUninitialized: true,
+  resave: true,
+  env: 'dev'
+}));
+
+
+// Front view
+app.get('/', function (req, res) {
+  res.render('index', {
+    page: 'front'
+  });
+});
+
+app.use('/slack-chat', function (req, res) {
+  console.log(req.body);
+});
+
+
+
+var io = require('socket.io').listen(app.listen(app.get('port')));
+
+io.sockets.on('connection', function (socket) {
+
+  socket.on('message', function (data) {
+    
+
+    request({
+      uri: 'https://hooks.slack.com/services/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',
+      method: 'POST',
+      body: JSON.stringify({
+        username: data.username || 'Daytona Chat',
+        text: data.message,
+        channel: 'slacks'
+      })
+    }, function (error, response, body) {
+      if (error) {
+        console.log('Error', error);
+        return;
+      }
+
+      console.log(response, body);
+
+      socket.emit('chat', {
+        message: data.message
+      });
+    });
+
+  });
+
+});
